53b44269fdd8d603e7408a8eb5f6e8393aeec429
Jakob Langenbahn <accounts@jakoblangenbahn.com>
2023-02-11 18:52:08 +0100

    welcome component

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="ligth">
       <Container>
-        <Logo style={{ maxWidth: '10rem', maxHeight: '2rem' }} />
+        <Logo alt={title} style={{ maxWidth: '10rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );

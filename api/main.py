4b47257ee83cc94e33ab04440bb523cc233fff12
Jakob Langenbahn <accounts@jakoblangenbahn.com>
2023-03-27 12:55:13 +0200

    enabled pylint and black code formatter in python

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import requests
 from dotenv import load_dotenv
 from flask_cors import CORS
@@ -6,33 +6,32 @@ from flask import Flask, request
 
 load_dotenv(dotenv_path=".env.local")
 
-UNSPLASH_URL="https://api.unsplash.com/photos/random/"
+UNSPLASH_URL = "https://api.unsplash.com/photos/random/"
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
+    raise EnvironmentError(
+        "Please create .env.local file and insert there UNSPLASH_KEY"
+    )
 
 app = Flask(__name__)
 CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
+
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
 
-    headers = {
-        "Authorization": f"Client-ID {UNSPLASH_KEY}", 
-        "Accept-Version": "V1"
-               }
-    params = {
-        "query": word
-        }
+    headers = {"Authorization": f"Client-ID {UNSPLASH_KEY}", "Accept-Version": "V1"}
+    params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
 
     data = response.json()
     return data
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
